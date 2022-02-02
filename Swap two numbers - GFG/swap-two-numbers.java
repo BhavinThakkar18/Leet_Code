0602199c0ceaf08a8994abb8e3035401c8558856
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-02-02 16:02:54 +0530

    Added solution - LeetHub

---
+++ Swap two numbers - GFG/swap-two-numbers.java
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            Solution ob = new Solution();
+            List<Integer> ans = ob.get(a,b);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        List<Integer> ans= new ArrayList();
+        ans.add(a+b-a);
+        ans.add(b+a-b);
+        return ans;
+        
+    }
+}
