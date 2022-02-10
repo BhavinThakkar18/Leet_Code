8bc4df06047e9c21d030312f9e268c2f651c6eb7
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-02-10 12:34:47 +0530

    Added solution - LeetHub

---
+++ XOR Game - GFG/xor-game.java
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int k = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.xorCal(k));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int xorCal(int k){
+    if(k==1) return 2;
+    if(((k&(k+1))==0)) return k/2;
+    return -1;
+    }
+}
