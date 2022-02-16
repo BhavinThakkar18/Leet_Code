bef7b111054b0f10139eb08e79332549af673401
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-02-16 21:32:40 +0530

    Added solution - LeetHub

---
+++ Josephus problem - GFG/josephus-problem.java
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Recursion {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();//testcases
+		while(T-->0)
+		{
+		    Solution obj=new Solution();
+		    
+		    int n,k;
+		    //taking input n and k
+		    n=sc.nextInt();
+		    k=sc.nextInt();
+		    
+		    //calling josephus() function
+		    System.out.println(obj.josephus(n,k));
+		    
+		    
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+   public int josephus(int n, int k)
+    {
+       if(n==1) return n;
+       int t = (k+josephus(n-1,k))%n;
+       if(t==0) return n;
+       else return t;
+    }
+}
+
