0dd4c9857f19b92e95ee8c79b7e7125170470f6e
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-02-23 13:16:54 +0530

    Added solution - LeetHub

---
+++ Insertion Sort for Singly Linked List - GFG/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        Node dummy=new Node(100000);
+        while(head_ref!=null)
+        {
+            Node next=head_ref.next;
+            Node temp=dummy;
+            while((temp.next!=null) && (temp.next.data < head_ref.data))
+            {
+                temp=temp.next;
+            }
+            head_ref.next=temp.next;
+            temp.next=head_ref;
+            head_ref=next;
+        }
+        return dummy.next;
+    }
+}
