e727cdef7dbbc95a0717b633eef2f27ff34684bc
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-03-24 17:00:31 +0530

    Added solution - LeetHub

---
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException{
+		
+		//taking input using BufferedReader class
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//taking total count of testcases
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    //Reading the two Strings
+		    String s1 = br.readLine();
+		    String s2 = br.readLine();
+		    
+		    //Creating an object of class Rotate
+		    Solution obj = new Solution();
+		    
+		    //calling areRotations method 
+		    //of class Rotate and printing
+		    //"1" if it returns true
+		    //else "0"
+		    if(obj.areRotations(s1,s2))
+		    {
+		        System.out.println("1");
+		    }
+		    else
+		    {
+		        System.out.println("0");
+		    }
+		}
+	}
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2 )
+    {
+        if(s1.length()!=s2.length())
+        {
+            return false;
+        }
+        if(s1.equals(s2))
+        {
+            return true;
+        }
+        String temp=s1+s1;
+        if(temp.contains(s2)==true)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+}
