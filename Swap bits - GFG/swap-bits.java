8513840de0160138c04e1c6957266f2484e35d31
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-03-28 10:59:37 +0530

    Added solution - LeetHub

---
+++ Swap bits - GFG/swap-bits.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int X = Integer.parseInt(input_line[0]);
+            int P1 = Integer.parseInt(input_line[1]);
+            int P2 = Integer.parseInt(input_line[2]);
+            int N = Integer.parseInt(input_line[3]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.swapBits(X, P1, P2, N));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    int swapBits(int x, int p1, int p2, int n)
+    {
+        int set1 = (x>>p1) & ((1<<n)-1);
+        int set2 = (x>>p2) & ((1<<n)-1);
+        int xor = (set1^set2);
+        xor = (xor<<p1) | (xor<<p2);
+        return x ^ xor;
+    }
+}
