0e8b15a90f355d755c4cc97506a27d069cf60aad
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-03-29 13:59:33 +0530

    Time: 33 ms (27.14%), Space: 75.1 MB (74.97%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,16 +1,31 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int ans=0;
-        for(int i=0;i<nums.length-1;i++)
+    public boolean isCntOfElementsLessThanMid(int mid,int[] nums)
+    {
+        int cnt =0;
+        for(int i=0;i<nums.length;i++)
         {
-            int j=i+1;
-            if(nums[i]==nums[j])
+            if(nums[i]<=mid)
             {
-                ans=nums[i];
-                break;
+                cnt++;
             }
         }
-        return ans;
+        return (cnt<=mid);
+    }
+    public int findDuplicate(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(isCntOfElementsLessThanMid(mid,nums)==true)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        return l;
     }
 }
