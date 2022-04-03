0e950bea171e6b79546ad1105c9bd63736edf551
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-04-03 07:49:32 -0400

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+
+class GFG
+{
+    ArrayList<Long> find(long arr[], int n, int x)
+    {
+        ArrayList<Long> ans = new ArrayList<>();
+        int i=0;
+        int j=0;
+        long first=-1;
+        long last=-1;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]==(long)x)
+            {
+               first=i;
+               break;
+            }
+        }
+        for(j=n-1;j>=0;j--)
+        {
+            if(arr[j]==(long)x)
+            {
+                last=j;
+                break;
+            }
+        }
+        ans.add(first);
+        ans.add(last);
+        return ans;
+    }
+}
+
+
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long arr[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Long> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+  // } Driver Code Ends
