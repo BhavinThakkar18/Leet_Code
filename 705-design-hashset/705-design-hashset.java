a3a5c43668ed2639006b46d9068f67eaf5b8cdcb
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-04-21 19:20:30 +0530

    Time: 39 ms (39.12%), Space: 56.3 MB (64.42%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,20 +1,44 @@
 class MyHashSet {
-    
-    boolean arr[];
-    public MyHashSet() {
-        arr=new boolean[1000001];
-    }
-    
+        private final int MAX_VALUE=1000000;
+        private final int ARRAY_SIZE=100;
+        public List<List<Integer>> parentList;
+       public MyHashSet() {
+           parentList = new ArrayList<>(ARRAY_SIZE);
+            for(int i=0;i<ARRAY_SIZE;i++)
+            {
+                parentList.add(null);
+            }
+       }
     public void add(int key) {
-        arr[key]=true;
+        int index=key % ARRAY_SIZE;
+        List<Integer> childList=parentList.get(index);
+        if(childList==null)
+        {
+            List<Integer> list= new LinkedList<>();
+            list.add(key);
+            parentList.set(index,list);
+        }
+        else
+        {
+            if(!childList.contains(key))
+            {
+                childList.add(key);
+            }
+        }
     }
-    
     public void remove(int key) {
-        arr[key]=false;
+        int index=key%ARRAY_SIZE;
+        List<Integer>childList=parentList.get(index);
+        if(childList!=null)
+        {
+            childList.remove(Integer.valueOf(key));
+        }
     }
     
     public boolean contains(int key) {
-        return arr[key];
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList=parentList.get(index);
+        return childList!=null && childList.contains(key);
     }
 }
 
