ec6b5e96d6d72d51ae5d43138b6564d103f1dfa2
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-05-01 17:37:32 +0530

    Time: 2 ms (9.76%), Space: 41.6 MB (66.39%) - LeetHub

---
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        int[][] mat=new int[8][8];
+        //0=white=true
+        //1=black==false
+        for(int i=0;i<8;i++)
+        {
+            for(int j=7;j>=0;j--)
+            {
+                if((i+j)%2==0)
+                {
+                    mat[i][j]=0;
+                }
+                else
+                {
+                    mat[i][j]=1;
+                }
+            }
+        }
+        // for(int i=0;i<8;i++)
+        // {
+        //     for(int j=0;j<8;j++)
+        //     {
+        //         System.out.print(mat[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        int col=coordinates.charAt(0)-'a';
+        int row=coordinates.charAt(1)-'1';
+        row=7-row;
+        //System.out.println("row: "+row+" "+"col: "+col);
+        return mat[row][col]==1?false:true;
+    }
+}
