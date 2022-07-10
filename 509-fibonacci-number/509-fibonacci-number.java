f3aa6112853506d9b3d258d829238617689bc941
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-07-10 17:45:00 +0530

    Time: 8 ms (36.50%), Space: 39 MB (94.09%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,6 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
-        int last = fib(n-1);
-        int secondLast=fib(n-2);
-        return last+secondLast;
+        return fib(n-1)+fib(n-2);
     }
 }
