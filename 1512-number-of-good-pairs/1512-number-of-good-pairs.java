20067c2999e2fbb184241c196654a43208aab326
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-09-04 16:08:29 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (25.02%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,21 +1,24 @@
 class Solution {
+    public int findFactorial(int n)
+    {
+        if(n<=1)
+        {
+            return 0;
+        }
+        return ((n)*(n-1))/2;
+    }
     public int numIdenticalPairs(int[] nums) {
-        HashMap<Integer,Integer> hashMap = new HashMap<Integer,Integer>();
-        int countPair=0;
+        int ans=0;
+        int[] freq = new int[101];
         for(int i=0;i<nums.length;i++)
         {
-            if(hashMap.containsKey(nums[i]))
-            {
-                int val = hashMap.get(nums[i]);
-                countPair+=val;
-                hashMap.put(nums[i],val+1);
-                
-            }
-            else
-            {
-                hashMap.put(nums[i],1);
-            }
+            freq[nums[i]]+=1;
         }
-        return countPair;
+        for(int i=1;i<100;i++)
+        {
+            //System.out.print(freq[i]+" ");
+            ans+=findFactorial(freq[i]);
+        }
+        return ans;
     }
 }
