a2c8e674ab535f8beb2dae9877f74c6523561877
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-09-20 10:48:37 +0530

    Time: 20 ms (5.26%), Space: 42.4 MB (75.36%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxPower(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        stack.push(s.charAt(0));
+        int ans=1;
+        for(int i=1;i<s.length();i++)
+        {
+            
+            if(stack.size()>0)
+            {
+                if(s.charAt(i)==stack.peek())
+                {
+                    stack.push(s.charAt(i));
+                    ans=Math.max(ans,stack.size());
+                
+                }
+                else
+                {
+                    while(!stack.isEmpty())
+                    {
+                        stack.pop();   
+                    }
+                    stack.push(s.charAt(i));
+                }
+            }
+            else
+            {
+                stack.push(s.charAt(i));
+                ans=Math.max(ans,stack.size());
+            }
+            
+        }
+        return ans;
+    }
+}
